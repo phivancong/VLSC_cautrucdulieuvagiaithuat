b0c8bcc98f2254ace1b6e475af0724f44f5b43fe
phivancong <2500116169@vanlangsaigon.edu.vn>
2026-04-06 10:37:11 +0700

    Hoan thanh lab 6

---
+++ DSA_Session06_Basics/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DSA_Session06_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            
+            do
+            {
+                Console.WriteLine("\n=== MENU QUẢN LÝ ===");
+                Console.WriteLine("1. Chức năng A");
+                Console.WriteLine("2. Chức năng B");
+                Console.WriteLine("0. Thoát chương trình");
+                Console.Write("Mời bạn chọn (0-2): ");
+
+                choice = int.TryParse(Console.ReadLine(), out choice)
+                    ? choice
+                    : -1; 
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Đang chạy chức năng A...");
+                        break;
+                    case 2:
+                        Console.WriteLine("Đang chạy chức năng B...");
+                        break;
+                    case 0:
+                        Console.WriteLine("Tạm biệt!");
+                        break;
+                    default:
+                        Console.WriteLine("Lựa chọn không hợp lệ, vui lòng nhập lại!");
+                        break;
+                }
+            } while (choice != 0);
+        }
+    }
+}
